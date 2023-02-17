8b4b9a4bf82810cc6ef741f36c10a4958ee1b4e0
vigovlugt <vigovlugt@gmail.com>
2023-02-17 10:46:35 +0100

    Add copilot and save keymap

---
+++ nvim/.config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,2 @@
+vim.g.copilot_assume_mapped = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
--- nvim/.config/nvim/lua/vigo/remap.lua
+++ nvim/.config/nvim/lua/vigo/remap.lua
@@ -29,3 +29,5 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "<leader>s", "<cmd>LspZeroFormat<CR><cmd>w<CR>")
+
